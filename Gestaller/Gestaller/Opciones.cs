ac6d9d9bfb39a25d1a85477aa3c5e83134d43dd8
TermiSenpai <escarpaprieto@gmail.com>
2021-09-30 12:17:57 +0200

    Business data design

--- Gestaller/Gestaller/Opciones.cs
+++ Gestaller/Gestaller/Opciones.cs
@@ -16,5 +16,15 @@ namespace Gestaller
         {
             InitializeComponent();
         }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void namePanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
