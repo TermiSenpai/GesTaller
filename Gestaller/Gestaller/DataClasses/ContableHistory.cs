5bb941ec6f8ee6b830d4f91b3ab09d68fb23cf0d
Carlos Mestre <CARMESCE@gmail.com>
2021-10-25 16:44:22 +0200

    Classes -> DataClasses

--- Gestaller/Gestaller/DataClasses/ContableHistory.cs
+++ Gestaller/Gestaller/DataClasses/ContableHistory.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gestaller.Classes
+namespace Gestaller
 {
-    class InoviceHistory
+    class ContableHistory
     {
     }
 }
--- Gestaller/Gestaller/DataClasses/Contact.cs
+++ Gestaller/Gestaller/DataClasses/Contact.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gestaller.Classes
+namespace Gestaller
 {
     public struct Contact
     {
