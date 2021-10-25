d3a13989541113b76cd52390f7f6032322bb884f
Termi <escarpaprieto@gmail.com>
2021-10-25 18:05:02 +0200

    taxRate dataClass

---
+++ Gestaller/Gestaller/DataClasses/TaxRate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestaller.DataClasses
+{
+    public class TaxRate
+    {
+        TaxRate() { }
+
+        TaxRate(int value, string description)
+        {
+            this.value = value;
+            this.description = description;
+        }
+
+        public int value { get; set; }
+        public string description { get; set; }
+    }
+}
