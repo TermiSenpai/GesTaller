d76b9ab35141b32708309f335aa1c139ae59b94e
Termi <escarpaprieto@gmail.com>
2021-10-25 19:00:36 +0200

    dataDummieLayer

---
+++ Gestaller/Gestaller/Layers/DataLayerDummie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestaller.Layers
+{
+    class DataLayerDummie
+    {
+        public List<Contact> GetContacts()
+        {
+            List<Contact> contacts = new List<Contact>();
+            Contact firstContact = new Contact();
+            firstContact.id = 1;
+            firstContact.company = "company1";
+            firstContact.fullName = "Roberto Sencillo";
+            firstContact.cif = "12312321vad";
+            firstContact.cp = 199903;
+            firstContact.address = "General Ramón yo que se";
+            firstContact.city = "Guadalajara";
+            firstContact.district = "Guadalajara";
+            firstContact.email = "asdfas@gmail.com";
+            firstContact.fax = "nosequesunfax";
+            firstContact.phone = "989898123";
+            firstContact.mobile = "91382411231";
+
+            contacts.Add(firstContact);
+            return contacts;
+        }
+    }
+}
