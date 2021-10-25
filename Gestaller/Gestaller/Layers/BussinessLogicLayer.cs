2482889401097b6c8819ba48d61e19f8b281223b
Termi <escarpaprieto@gmail.com>
2021-10-25 19:00:24 +0200

    bussinessLogicLayer

---
+++ Gestaller/Gestaller/Layers/BussinessLogicLayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestaller.Layers
+{
+    class BussinessLogicLayer
+    {
+        private DataLayerDummie _dataLayerDummie;
+        public BussinessLogicLayer()
+        {
+            _dataLayerDummie = new DataLayerDummie();
+        }
+        public List<Contact> GetContacts()
+        {
+            return _dataLayerDummie.GetContacts();
+        }
+    }
+}
