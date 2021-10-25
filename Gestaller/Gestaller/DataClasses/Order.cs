d2664d2078d6b7393c781436dbfbf7c3266c4465
Termi <escarpaprieto@gmail.com>
2021-10-25 18:04:34 +0200

    Orders dataClasses

--- Gestaller/Gestaller/DataClasses/Order.cs
+++ Gestaller/Gestaller/DataClasses/Order.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Gestaller
 {
-    class Order
+    public class Order
     {
+        public Order() { }
+
+        public Order(int status, int numBudget, int numProForma, int numInvoice,
+                    DateTime dateBudget, DateTime dateProForma, DateTime dateInvoice)
+        {
+            this.status = status;
+            this.numBudget = numBudget;
+            this.numProForma = numProForma;
+            this.numInvoice = numInvoice;
+            this.dateBudget = dateBudget;
+            this.dateProForma = dateProForma;
+            this.dateInvoice = dateInvoice;
+        }
+
+        public int status                  { get; set; }
+        public int numBudget               { get; set; }
+        public int numProForma             { get; set; }
+        public int numInvoice              { get; set; }
+        public DateTime dateBudget         { get; set; }
+        public DateTime dateProForma       { get; set; }
+        public DateTime dateInvoice        { get; set; }
+
     }
 }
---
+++ Gestaller/Gestaller/DataClasses/Order2Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestaller.DataClasses
+{
+    public class Order2Item
+    {
+        Order2Item() { }
+
+        Order2Item(int quantity, float basePrice, float taxRate, int discount)
+        {
+            this.quantity = quantity;
+            this.basePrice = basePrice;
+            this.taxRate = taxRate;
+            this.discount = discount;
+        }
+
+        public int quantity         { get; set; }
+        public float basePrice      { get; set; }
+        public float taxRate        { get; set; }
+        public int discount         { get; set; }
+    }
+}
