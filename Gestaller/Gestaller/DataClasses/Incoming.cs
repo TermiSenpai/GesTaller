5d2f7dd5b3c7987733f5d85812aa8d311c969100
Termi <escarpaprieto@gmail.com>
2021-10-25 18:02:13 +0200

    incoming public data

--- Gestaller/Gestaller/DataClasses/Incoming.cs
+++ Gestaller/Gestaller/DataClasses/Incoming.cs
@@ -10,20 +10,20 @@ namespace Gestaller
     {
         public Incoming() { }
 
-        public Incoming(DateTime receiptDate, DateTime departureDate,
+        public Incoming(DateTime incomingDate, DateTime departureDate,
                         DateTime estimatedDate, string damages, float kms)
         {
-            this.receiptDate = receiptDate;
+            this.incomingDate = incomingDate;
             this.departureDate = departureDate;
             this.estimatedDate = estimatedDate;
             this.damages = damages;
             this.kms = kms;
         }
-        public DateTime receiptDate { get; set; }
-        public DateTime departureDate { get; set; }
-        public DateTime estimatedDate { get; set; }
-        public string damages { get; set; }
-        public float kms { get; set; }
+        public DateTime incomingDate    { get; set; }
+        public DateTime departureDate   { get; set; }
+        public DateTime estimatedDate   { get; set; }
+        public string damages           { get; set; }
+        public float kms                { get; set; }
 
     }
 }
