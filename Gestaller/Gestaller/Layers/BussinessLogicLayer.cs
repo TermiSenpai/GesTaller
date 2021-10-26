1c4dd5d96273bf3da159f7197b637146c18a5715
Termi <escarpaprieto@gmail.com>
2021-10-26 16:51:28 +0200

    getVehicles

--- Gestaller/Gestaller/Layers/BussinessLogicLayer.cs
+++ Gestaller/Gestaller/Layers/BussinessLogicLayer.cs
@@ -17,5 +17,19 @@ namespace Gestaller.Layers
         {
             return _dataLayerDummie.GetContacts();
         }
+
+        public List<Vehicle> GetVehicles()
+        {
+            return _dataLayerDummie.GetVehicles();
+        }
+
+        public List<ContactVehicle> GetContactVehicles()
+        {
+            List<ContactVehicle> contactVehicle = new List<ContactVehicle>();
+            List<Contact> contacts = _dataLayerDummie.GetContacts();
+            List<Vehicle> vehicles = _dataLayerDummie.GetVehicles();
+            contactVehicle.Add(new ContactVehicle(contacts[0],vehicles[0]));
+            return contactVehicle;
+        }
     }
 }
