1470ed1ee06b2f72e1a13bb722c256a0d147d4a9
Termi <escarpaprieto@gmail.com>
2021-10-26 16:51:04 +0200

    ContactVehicle dataClass

---
+++ Gestaller/Gestaller/DataClasses/ContactVehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestaller
+{
+    public class ContactVehicle
+    {
+        public ContactVehicle(Contact contact, Vehicle vehicle) 
+        {
+            this.contact = contact;
+            this.vehicle = vehicle;
+        }
+
+        public Contact contact { get; set; }
+        public Vehicle vehicle { get; set; }
+    }
+}
