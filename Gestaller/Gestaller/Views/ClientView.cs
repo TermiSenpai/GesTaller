9499af5ed227de344c9c0a3d03a813b6af008b07
Termi <escarpaprieto@gmail.com>
2021-10-26 16:51:17 +0200

    getDB edited

--- Gestaller/Gestaller/Views/ClientView.cs
+++ Gestaller/Gestaller/Views/ClientView.cs
@@ -32,8 +32,20 @@ namespace Gestaller
 
         private void getDB()
         {
-            List<Contact> contacts = _bussinessLogicLayer.GetContacts();
-            dataGrid.DataSource = contacts;
+           /* 
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Datos1", typeof(int));
+            tabla.Columns.Add("Datos2", typeof(string));
+            tabla.Columns.Add("Fecha", typeof(DateTime));
+            foreach (DataRow row in dataGrid.Rows)
+            {
+                tabla.Rows.Add(row["Datos1"], row["Datos2"], row["Fecha"]);
+            }
+            dataGrid.DataSource = tabla;*/
+
+            List<ContactVehicle> contactVehicle = _bussinessLogicLayer.GetContactVehicles();
+
+            dataGrid.DataSource = contactVehicle;
         }
     }
 }
