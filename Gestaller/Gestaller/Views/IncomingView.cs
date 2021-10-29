fc93179e14d8cf82ee0b6642454719f00e6e41c4
Termi <escarpaprieto@gmail.com>
2021-10-29 17:07:54 +0200

    Incoming item set

--- Gestaller/Gestaller/Views/IncomingView.cs
+++ Gestaller/Gestaller/Views/IncomingView.cs
@@ -21,6 +21,7 @@ namespace Gestaller
         private void IncomingView_Load(object sender, EventArgs e)
         {
             getDB();
+            incomingItemSet();
         }
 
         private void getDB()
@@ -28,6 +29,32 @@ namespace Gestaller
             List<Incoming> incomings = _bussinessLogicLayer.GetIncomings();
 
             dataGridViewDepositos.DataSource = incomings;
+        }
+
+        private void incomingItemSet()
+        {
+            List<ContactVehicle> contactsVehicles = _bussinessLogicLayer.GetContactVehicles();
+
+            for (int i = 0; i < contactsVehicles.Count(); i++)
+            {
+                #region contacto
+                // Nombre
+                cueComboBoxCliente.Items.Add(contactsVehicles[i].contact_fullName);
+                #endregion
+
+                #region vehiculo
+                // Matricula
+                cueComboBoxMatricula.Items.Add(contactsVehicles[i].vehicle_enroll);
+                // Marca
+                cueComboBoxMarca.Items.Add(contactsVehicles[i].vehicle_brand);
+                // Modelo
+                cueComboBoxModelo.Items.Add(contactsVehicles[i].vehicle_model);
+                // Bastidor
+                cueComboBoxBastidor.Items.Add(contactsVehicles[i].vehicle_frame);
+                // Tipo motor
+                cueComboBoxTipoMotor.Items.Add(contactsVehicles[i].vehicle_engineType);
+                #endregion
+            }
         }
     }
 }
