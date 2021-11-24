3456e27f17a3821e4d9344870f0093d9836cd146
Termi <escarpaprieto@gmail.com>
2021-11-24 17:41:42 +0100

    iva data class

---
+++ Gestaller/Gestaller/DataClasses/Iva.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestaller.DataClasses
+{
+    class Iva
+    {
+        Iva()
+        {
+
+        }
+
+        Iva(int iva, string description)
+        {
+            this.iva = iva;
+            this.description = description;
+        }
+
+        int iva                 { get; set; }
+        string description      { get; set; }
+
+    }
+}
